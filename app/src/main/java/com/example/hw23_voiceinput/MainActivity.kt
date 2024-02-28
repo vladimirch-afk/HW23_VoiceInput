dbf4299b287782ad5f2ab07b1ca5605606c6f172
vlad <vladimir.chechurov@gmail.com>
2024-02-28 20:00:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw23_voiceinput/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.hw23_voiceinput
+
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Locale
+import java.util.Objects
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var iv_mic: Button? = null
+    private var tv_Speech_to_text: TextView? = null
+    private val REQUEST_CODE_SPEECH_INPUT = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        iv_mic = findViewById<Button>(R.id.button)
+        tv_Speech_to_text = findViewById<TextView>(R.id.text)
+        iv_mic?.setOnClickListener(View.OnClickListener {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.getDefault()
+            )
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text")
+            try {
+                startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+            } catch (e: Exception) {
+                Toast
+                    .makeText(
+                        this@MainActivity, " " + e.message,
+                        Toast.LENGTH_SHORT
+                    )
+                    .show()
+            }
+        })
+    }
+
+    override fun onActivityResult(
+        requestCode: Int, resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
+            if (resultCode == RESULT_OK && data != null) {
+                val result = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS
+                )
+                tv_Speech_to_text!!.text =
+                    Objects.requireNonNull(result)!![0]
+            }
+        }
+    }
+}
